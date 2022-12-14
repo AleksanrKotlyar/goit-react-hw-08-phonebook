e47d43fe73fc5eba77b56a00f3b64fe9ecfaffb7
AleksanrKotlyar <kotlyar.alek@gmail.com>
2022-12-14 15:50:31 +0200

    common

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
+import './components/Utils/index.css';
+import { theme } from './components/Utils/theme';
+import { ThemeProvider } from 'styled-components';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
